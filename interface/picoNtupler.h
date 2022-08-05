be8f8a7c2d182e1bdb79ef739a45dac8efedbb9d
Vinaya <vinaykrishnanmb@gmail.com>
2022-08-05 21:31:12 +0200

    T&P added

--- interface/picoNtupler.h
+++ interface/picoNtupler.h
@@ -13,10 +13,16 @@ using RNode = ROOT::RDF::RNode;
 using Vec_t = const ROOT::RVec<float>&;
 using Vec_i = const ROOT::RVec<int>&;
 
-float ZInvariantMass(int tau_i,int mu_i,Vec_t pt_1, Vec_t eta_1, Vec_t phi_1, Vec_t mass_1,Vec_t pt_2, Vec_t eta_2, Vec_t phi_2, Vec_t mass_2) {
-    const ROOT::Math::PtEtaPhiMVector p1(pt_1[tau_i], eta_1[tau_i], phi_1[tau_i], mass_1[tau_i]);
-    const ROOT::Math::PtEtaPhiMVector p2(pt_2[mu_i], eta_2[mu_i], phi_2[mu_i], mass_2[mu_i]);
-    return (p1 + p2).M();
+float ZMass(TLorentzVector tau_p4,TLorentzVector muon_p4) {
+
+    return (tau_p4 + muon_p4).M();
+}
+float CalcMT(TLorentzVector lep_p4,float met_pt,float met_phi){
+  TLorentzVector met_p4;
+  met_p4.SetPtEtaPhiE(met_pt,0,met_phi,0);
+  const double delta_phi = ROOT::Math::VectorUtil::DeltaPhi(lep_p4, met_p4);
+  return std::sqrt( 2.0 * lep_p4.Pt() * met_p4.Pt() * ( 1.0 - std::cos(delta_phi) ) );
+
 }
 float deltaR(float eta_1, float eta_2, float phi_1, float phi_2){
    const float deta = eta_1 - eta_2;
@@ -25,11 +31,11 @@ float deltaR(float eta_1, float eta_2, float phi_1, float phi_2){
 
    return sqrt(dRsq);
 }
-bool PassTagFilter(UInt_t ntrig,Vec_i trig_id,Vec_i trig_bits,Vec_t trig_pt,Vec_t trig_eta,Vec_t trig_phi,float tau_pt,float tau_eta,float tau_phi){
+bool PassTagFilter(UInt_t ntrig,Vec_i trig_id,Vec_i trig_bits,Vec_t trig_pt,Vec_t trig_eta,Vec_t trig_phi,float muon_pt,float muon_eta,float muon_phi){
    
    for(int it=0; it < ntrig; it++){
      const ROOT::Math::PtEtaPhiMVector trig(trig_pt[it],trig_eta[it],trig_phi[it],0);
-     float dR = deltaR(trig.Eta(),tau_eta,trig.Phi(),tau_phi);
+     float dR = deltaR(trig.Eta(),muon_eta,trig.Phi(),muon_phi);
      if (dR < 0.5){
       if((trig_bits[it] & 2) != 0 && trig_id[it] == 13){
            return true;
@@ -44,6 +50,8 @@ int MuonIndex(UInt_t ntrig,Vec_i trig_id,Vec_i trig_bits,Vec_t trig_pt,Vec_t tri
     for(int imu=0; imu <= nMu; imu++){
       const ROOT::Math::PtEtaPhiMVector muon(pt_1[imu], eta_1[imu], phi_1[imu], mass_1[imu]);
       float mu_iso = pfIso[imu]/muon.Pt();
+      if(muon.Pt() < 24)continue;
+      if(std::fabs(muon.Eta()) > 2.1)continue;
       if(mu_iso > 0.1)continue;
       if(!PassTagFilter(ntrig,trig_id,trig_bits,trig_pt,trig_eta,trig_phi,muon.Pt(),muon.Eta(),muon.Phi()))continue;
       mu_index = imu;
@@ -51,16 +59,15 @@ int MuonIndex(UInt_t ntrig,Vec_i trig_id,Vec_i trig_bits,Vec_t trig_pt,Vec_t tri
   }
   return mu_index;
 }
-int TauIndex(UInt_t ntau,Vec_t pt_1, Vec_t eta_1, Vec_t phi_1, Vec_t mass_1,Vec_t dz_1,UInt_t nmuon,int muon_intex,Vec_t pt_2, Vec_t eta_2, Vec_t phi_2, Vec_t mass_2){
+int TauIndex(UInt_t ntau,Vec_t pt_1, Vec_t eta_1, Vec_t phi_1, Vec_t mass_1,Vec_t dz_1,TLorentzVector muon_p4){
     int tau_index = -1;
-    if(nmuon > 0  && ntau > 0 && muon_intex > 0){
-      const ROOT::Math::PtEtaPhiMVector muon(pt_2[muon_intex], eta_2[muon_intex], phi_2[muon_intex], mass_2[muon_intex]);
+    if(ntau > 0){
       for (int itau = 0; itau <= ntau; itau++){
 	const ROOT::Math::PtEtaPhiMVector tau(pt_1[itau], eta_1[itau], phi_1[itau], mass_1[itau]);
 	if(dz_1[itau] > 0.2)continue;
-	if(deltaR(tau.Eta(),muon.Eta(),tau.Phi(),muon.Phi()) < 0.5)continue;
+	if(deltaR(tau.Eta(),muon_p4.Eta(),tau.Phi(),muon_p4.Phi()) < 0.5)continue;
 	if(tau.Pt() < 18) continue;
-	//if(std::fabs(tau.Eta()) > 2.5)continue; 
+	if(std::fabs(tau.Eta()) > 2.1)continue; 
 	tau_index = itau;
 	
       }  
@@ -154,4 +161,17 @@ bool PassBtagVeto(TLorentzVector muon_p4, TLorentzVector tau_p4,UInt_t njet, Vec
     }
   }
   return false;
+}
+float TauL1_PT(UInt_t ntrig,Vec_i trig_id,Vec_i trig_bits,Vec_t trig_l1pt,Vec_t trig_pt,Vec_t trig_eta,Vec_t trig_phi,float tau_pt,float tau_eta,float tau_phi){
+  float taul1_pt = 0;
+   for(int it=0; it < ntrig; it++){
+     const ROOT::Math::PtEtaPhiMVector trig(trig_pt[it],trig_eta[it],trig_phi[it],0);
+     float dR = deltaR(trig.Eta(),tau_eta,trig.Phi(),tau_phi);
+     if (dR < 0.5){
+       if( trig_id[it] == 15){ 
+	 taul1_pt = trig_l1pt[it];
+      }
+   }
+  }
+  return taul1_pt;
 }
---
+++ producer/picoNtupler_TandP.py
@@ -0,0 +1,93 @@
+import os
+import sys
+
+import ROOT
+
+# Enable multi-threading                                                                                                                                                                                    
+ROOT.ROOT.EnableImplicitMT()
+
+
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Skim full tuple.')
+parser.add_argument('--input', required=False, type=str, nargs='+', help="input files")
+parser.add_argument('--channel', required=True, type=str, help="ditau,mutau or etau")
+parser.add_argument('--run', required=True, type=str, help="tau selection")
+parser.add_argument('--plot', required=True, type=str, help="plot name")
+parser.add_argument('--iseta',action='store_true', help="plot name")
+parser.add_argument('--var', required=True, type=str, help="tau_pt or tau_eta")
+
+args = parser.parse_args()
+
+
+
+core_dir = str(os.getcwd()).split('producer')
+
+Trigger_header_path = os.path.join(core_dir[0] +'interface' + os.sep,"picoNtupler.h")
+
+ROOT.gInterpreter.Declare('#include "{}"'.format(Trigger_header_path))
+
+sys.path.insert(1, core_dir[0]+'/python')
+from RooPlottingTool import *
+
+
+inputFiles = (f'/eos/cms/store/group/dpg_trigger/comm_trigger/TriggerStudiesGroup/STEAM/anayak/2022NanoAOD/SingleMuonV1/Files2/nano_aod_{i}.root' for i in range(0,79))
+
+
+df = ROOT.RDataFrame("Events",inputFiles)
+
+# Tag And Probe selection {Obtaining high pure Z -> mu tau Events}
+
+## select muon (Tag) candidate 
+
+df_tag = df.Filter("nMuon == 1 && nTau >=1").Define("Muon_Index","MuonIndex(nTrigObj,TrigObj_id,TrigObj_filterBits,TrigObj_pt,TrigObj_eta,TrigObj_phi,nMuon,Muon_pt,Muon_eta,Muon_phi,Muon_mass,Muon_pfRelIso04_all)").Define("muon_p4","Muon_p4(Muon_Index,Muon_pt,Muon_eta,Muon_phi,Muon_mass)")
+
+## select tau (probe) candidate
+
+df_probe = df_tag.Filter("Muon_Index >= 0").Define("Tau_Index","TauIndex(nTau,Tau_pt,Tau_eta,Tau_phi,Tau_mass,Tau_dz,muon_p4)")
+
+df_probe_id = df_probe.Filter("Tau_Index >= 0 && Tau_idDeepTau2017v2p1VSjet[Tau_Index] >=16 && Tau_idDeepTau2017v2p1VSmu[Tau_Index] >=8 && Tau_idDeepTau2017v2p1VSe[Tau_Index] >=2").Define("tau_p4","Tau_p4(Tau_Index,Tau_pt,Tau_eta,Tau_phi,Tau_mass)")
+
+## select mu-tau pair with os and ss events
+df_TandP_os = df_probe_id.Define('weight',"(Tau_charge[Tau_Index] != Muon_charge[Tau_Index]) ? 1. : -1.").Define("mT","CalcMT(muon_p4,MET_pt,MET_phi)").Define("m_vis","ZMass(tau_p4,muon_p4)")
+
+## select pure Z -> mu tau events
+df_TandP = df_TandP_os.Filter("mT < 30 && m_vis > 40 && m_vis < 80").Define("tau_pt","Tau_pt[Tau_Index]").Define("tau_eta","Tau_eta[Tau_Index]")
+
+#print(ss_events,"   ",os_events)
+
+# Calculate Efficiency 
+h_num_os = df_TandP.Histo1D(CreateHistModel("numerator",args.iseta),args.var)
+h_den_os = df_TandP.Histo1D(CreateHistModel("denominator",args.iseta),args.var)
+
+
+#h_den_os.Add(h_den_ss.GetPtr(),-1)
+
+
+if(args.channel == 'ditau'):
+    df_TandP_num = df_TandP.Define("pass_ditau","PassDiTauFilter(nTrigObj,TrigObj_id,TrigObj_filterBits,TrigObj_pt,TrigObj_eta,TrigObj_phi,Tau_pt[Tau_Index],Tau_eta[Tau_Index],Tau_phi[Tau_Index])")
+    h_num_os = df_TandP_num.Filter("pass_ditau > 0.5 && HLT_IsoMu24_eta2p1_MediumDeepTauPFTauHPS35_L2NN_eta2p1_CrossL1 == 1").Histo1D(CreateHistModel("numerator",args.iseta),args.var,'weight')
+elif(args.channel == 'mutau'):
+    df_TandP_num = df_TandP.Define("pass_mutau","PassMuTauFilter(nTrigObj,TrigObj_id,TrigObj_filterBits,TrigObj_pt,TrigObj_eta,TrigObj_phi,Tau_pt[Tau_Index],Tau_eta[Tau_Index],Tau_phi[Tau_Index])")
+    h_num_os = df_TandP_num.Filter("pass_mutau > 0.5 && HLT_IsoMu20_eta2p1_LooseDeepTauPFTauHPS27_eta2p1_CrossL1==1").Histo1D(CreateHistModel("numerator",args.iseta),args.var,'weight')
+    
+#h_num_os.Add(h_num_ss.GetPtr(),-1)
+
+## Produce plot                 
+print("Create TurnON")                                                                                                                                                                         
+ROOT.gStyle.SetOptStat(0); ROOT.gStyle.SetTextFont(42)
+c = ROOT.TCanvas("c", "", 800, 700)
+gr = ROOT.TEfficiency(h_num_os.GetPtr(),h_den_os.GetPtr())
+gr.SetTitle("")
+gr.Draw()
+label = ROOT.TLatex(); label.SetNDC(True)
+if(args.var == "tau_pt" or args.var=="tau_l1pt"):
+    label.DrawLatex(0.8, 0.03, "#tau_pT")
+else:
+    label.DrawLatex(0.8, 0.03, "#eta_#tau")
+label.SetTextSize(0.040); label.DrawLatex(0.100, 0.920, "#bf{CMS Run3 Data}")
+label.SetTextSize(0.030); label.DrawLatex(0.630, 0.920, "#sqrt{s} = 13.6 TeV, "+args.run)
+
+c.SaveAs(args.plot+".pdf")
+
